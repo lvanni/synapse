af9ac72219a292c6f6a3d0fdcb074d64eaef273e
lvanni <lvanni@700752c6-984c-461e-9c28-e1bb893e44aa>
2009-12-04 11:22:26 +0000

    scripts for git are added
    git-svn-id: https://scm.gforge.inria.fr/svn/synapse@12 700752c6-984c-461e-9c28-e1bb893e44aa

--- jSynapse/trunk/src/core/synapse/ISynapse.java
+++ jSynapse/trunk/src/core/synapse/ISynapse.java
@@ -1,14 +1,8 @@
 package core.synapse;
 
-
 public interface ISynapse {
-	/** Code for synapse*/
-	public final static int OPE    = 3;
-	public final static int FIND   = 4; // FIND,<My???>,<id>
-	public final static int FOUND  = 5;
-	public final static int INVITE = 6;
-	public final static int JOIN   = 7;
 	
 	public void invite();
 	public void join(String host, int port);
+
 }
