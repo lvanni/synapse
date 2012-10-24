7a85111d11ced602d6c4522f0c7872e3c37db58f
romain <fritz.romain@etu.unice.fr>
2012-10-24 14:38:15 +0200

    finish request from oracle to SynapseSim Romain

--- jSynapse/trunk/src/experiment/simulation/Oracle.java
+++ jSynapse/trunk/src/experiment/simulation/Oracle.java
@@ -1,5 +1,9 @@
 package experiment.simulation;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 import core.protocol.p2p.Node;
 import core.protocol.transport.ITransport;
 import core.protocol.transport.socket.request.RequestHandler;
@@ -7,17 +11,43 @@ import core.protocol.transport.socket.server.SocketImpl;
 
 public class Oracle {
 	
-	public Oracle(){
-		
+	/**
+	 * Format the response to send to the synapse simulator
+	 * 
+	 * The response has this form:
+	 * command,protocol,networkID,additionnal_information,...
+	 * 
+	 * @param args
+	 * @return
+	 */
+	private static String formatResponse(String[] args){
+		String command="";
+		for(int i=0;i<args.length;i++){
+			if(i==0){
+				command=args[i];
+			}
+			else{
+				command+=","+args[i];
+			}
+		}
+		return command;
 	}
 	
-	
-	/**private void lauchCommandLine(){
-		while(true){
-			
+	/**
+	 * Print result of commands in a log file
+	 * @param log
+	 */
+	private static void printInLogsFile(String log){
+		File f = new File("log_synapse.txt");
+		PrintWriter pw=null;
+		try {
+			pw = new PrintWriter(f);
+		} 
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
 		}
-	}**/
-	
+		pw.println(log);
+	}
 	
 	/**
 	 * Main function to launch the application
@@ -31,8 +61,9 @@ public class Oracle {
 					10, 1, 100, null);
 			
 			Node simulator = new Node("localhost", SynapseSim.DEFAULT_PORT);
-			String reponse = transport.sendRequest("Hello", simulator);
-			System.out.println(reponse);
+			String response = transport.sendRequest(formatResponse(args), simulator);
+			System.out.println(response);
+			printInLogsFile(response);
 			
 		} catch (Exception e) {
 			e.printStackTrace();
