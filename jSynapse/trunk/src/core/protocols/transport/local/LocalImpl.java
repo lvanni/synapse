794f1e5ce8f76bea4a2d815dedd52cd2ab6246e4
Laurent Vanni <laurent.vanni@sophia.inria.fr>
2012-10-24 11:33:53 +0200

    FIX: adding files _LV

---
+++ jSynapse/trunk/src/core/protocols/transport/local/LocalImpl.java
@@ -0,0 +1,37 @@
+package core.protocols.transport.local;
+
+import simulation.ISimulator;
+import simulation.Simulator;
+import core.protocols.p2p.Node;
+import core.protocols.transport.ITransport;
+
+public class LocalImpl implements ITransport {
+	
+	public static int DEFAULT_PORT = 8000;
+	
+	private ISimulator simulator;
+	
+	public LocalImpl() {
+		this.simulator = Simulator.getInstance();
+	}
+
+	public int getPort() {
+		return DEFAULT_PORT;
+	}
+
+	/**
+	 * Send a message to the Node destination
+	 */
+	public String sendRequest(String message, Node destination) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * Stop the transport server
+	 */
+	public void stopServer() {
+		// nothing to do with local implementation
+	}
+
+}
---
+++ jSynapse/trunk/src/simulation/ISimulator.java
@@ -0,0 +1,12 @@
+package simulation;
+
+/**
+ * This interface define the Simulator
+ * 
+ * @author laurent.vanni@sophia.inria.fr - logNet team 2010 - INRIA
+ *         Sophia-Antipolis - France
+ * 
+ */
+public interface ISimulator {
+	
+}
