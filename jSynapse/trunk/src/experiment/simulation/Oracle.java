92494c826ead4b66390eb5b4fb184bc626c1213f
romain <fritz.romain@etu.unice.fr>
2012-10-25 16:14:36 +0200

    Oracle send numbers to command the simulator Romain

--- jSynapse/trunk/src/experiment/simulation/Oracle.java
+++ jSynapse/trunk/src/experiment/simulation/Oracle.java
@@ -3,11 +3,15 @@ package experiment.simulation;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.Locale;
 
 import core.protocol.p2p.Node;
 import core.protocol.transport.ITransport;
 import core.protocol.transport.socket.request.RequestHandler;
 import core.protocol.transport.socket.server.SocketImpl;
+import experiment.simulation.ISynapseSim.Command;
+import experiment.simulation.ISynapseSim.NodeType;
+import experiment.simulation.exception.SynapseSimException;
 
 public class Oracle {
 	
@@ -19,18 +23,70 @@ public class Oracle {
 	 * 
 	 * @param args
 	 * @return
+	 * @throws SynapseSimException 
 	 */
-	private static String formatResponse(String[] args){
+	private static String formatResponse(String[] args) throws SynapseSimException{
 		String command="";
 		for(int i=0;i<args.length;i++){
 			if(i==0){
-				command=args[i];
+				command=createCommand(args[i]).getValue()+"";
 			}
 			else{
+				if((i+1) % 2 == 0){
+					command+=","+createNodeType(args[i]).getValue();
+				}
 				command+=","+args[i];
 			}
 		}
 		return command;
+	}
+	
+	/**
+	 * Transform string in the code representing a command
+	 * to pass to the simulator
+	 * 
+	 * @param comm
+	 * @return
+	 * @throws SynapseSimException
+	 */
+	private static Command createCommand(String comm) throws SynapseSimException{
+		comm = comm.toLowerCase(Locale.ENGLISH);
+		if(comm.equals("create")){
+			return Command.CREATE;
+		}
+		if(comm.equals("put")){
+			return Command.PUT;
+		}
+		if(comm.equals("get")){
+			return Command.GET;
+		}
+		else{
+			throw new SynapseSimException("Unknown command in the oracle, maybe you wanted a coffee but sorry this program can't do that");
+		}
+	}
+	
+	/**
+	 * Transform string in the code representing a node type
+	 * to pass to the simulator
+	 * 
+	 * @param ntype
+	 * @return
+	 * @throws SynapseSimException
+	 */
+	private static NodeType createNodeType(String ntype) throws SynapseSimException{
+		ntype=ntype.toLowerCase(Locale.ENGLISH);
+		if(ntype.equals("chord")){
+			return NodeType.CHORD;
+		}
+		if(ntype.equals("kad")|| ntype.equals("kademlia")){
+			return NodeType.KAD;
+		}
+		if(ntype.equals("synapse")){
+			return NodeType.SYNAPSE;
+		}
+		else{
+			throw new SynapseSimException("Unknown node type in the oracle");
+		}
 	}
 	
 	/**
